64b76634c495cdf305336fa87514f9fd4b237d21
AL2002MI08 <alexandramizero@gmail.com>
2023-10-30 21:41:13 +0200

    initial commit

---
+++ src/components/Residencies/Residencies.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+//import {Swiper, SwiperSlider, useSwiper} from "swiper/react"
+//import "swiper/css"
+import "./Residencies.css"
+import data from '../../utils/slider.json'
+const Residencies = () => {
+    return (
+        <section className='r-wrapper'>
+            residencies
+        </section>
+    )
+}
+
+export default Residencies;
