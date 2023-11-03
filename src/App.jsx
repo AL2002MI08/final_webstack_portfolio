f5fd5f821ba867605fc379fa23b4f513574919fa
AL2002MI08 <alexandramizero@gmail.com>
2023-11-03 18:50:24 +0200

    merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import './App.css'
 import Companies from './components/Companies/Companies';
-import Residencies from './components/Residencies/Residencies';
-<<<<<<< HEAD
-import About from './components/About/About';
-=======
-import Value from './components/Value/Value';
-import Contact from './components/Contact/Contact'
->>>>>>> 4c6d2a001f460100e05484a10efc00bc5ad50a89
+import Residencies from './components/Residencies/Residencies';import About from './components/About/About';
 function App() {
   return (
     <div className="App">
@@ -19,12 +13,7 @@ function App() {
       </div>
       <Companies/>
       <Residencies/>
-<<<<<<< HEAD
       <About/>
-=======
-      <Value/>
-      <Contact/>
->>>>>>> 4c6d2a001f460100e05484a10efc00bc5ad50a89
     </div>
   );
 }
