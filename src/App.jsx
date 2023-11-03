cb60369dc3099ada97179c22900ab529ae713877
Alexandra_08 <105152394+AL2002MI08@users.noreply.github.com>
2023-11-03 18:52:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,7 @@ import Hero from './components/Hero/Hero';
 import './App.css'
 import Companies from './components/Companies/Companies';
 import Residencies from './components/Residencies/Residencies';
-<<<<<<< HEAD
 import About from './components/About/About';
-=======
-import Value from './components/Value/Value';
-import Contact from './components/Contact/Contact'
->>>>>>> 4c6d2a001f460100e05484a10efc00bc5ad50a89
 function App() {
   return (
     <div className="App">
@@ -19,12 +14,7 @@ function App() {
       </div>
       <Companies/>
       <Residencies/>
-<<<<<<< HEAD
       <About/>
-=======
-      <Value/>
-      <Contact/>
->>>>>>> 4c6d2a001f460100e05484a10efc00bc5ad50a89
     </div>
   );
 }
